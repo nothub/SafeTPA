71c9d4d040b8deae2792b94a49ad8d4586ae6b74
blockparole <48992448+blockparole@users.noreply.github.com>
2020-05-09 15:18:53 +0200

    dismout fix

--- src/main/java/not/hub/safetp/SafeTP.java
+++ src/main/java/not/hub/safetp/SafeTP.java
@@ -8,8 +8,11 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
 
 public final class SafeTP extends JavaPlugin {
 
