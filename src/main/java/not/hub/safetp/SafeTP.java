848a5d857b38d2ddbd1a00db075a971b3c997686
blockparole <48992448+blockparole@users.noreply.github.com>
2020-07-11 00:30:01 +0200

    better config handling

--- src/main/java/not/hub/safetp/SafeTP.java
+++ src/main/java/not/hub/safetp/SafeTP.java
@@ -18,11 +18,11 @@ public final class SafeTP extends JavaPlugin {
 
     private final RequestManager requestManager = new RequestManager();
 
-    private boolean multiRequest;
-    private int timeoutValue;
-    private boolean tpaFromSpawn;
-    private int spawnRadius;
-    private int unvanishDelay;
+    private boolean configMultiRequest;
+    private int configRequestTimeoutSeconds;
+    private int configUnvanishDelay;
+    private boolean configSpawnTpDeny;
+    private int configSpawnTpDenyRadius;
 
     static void sendMessage(Player player, String message) {
         player.sendMessage(message);
@@ -35,16 +35,19 @@ public final class SafeTP extends JavaPlugin {
 
         loadConfig();
 
-        multiRequest = getConfig().getBoolean("allow-multi-target-request");
-        tpaFromSpawn = getConfig().getBoolean("allow-tp-from-spawn");
-        timeoutValue = getConfig().getInt("request-timeout-seconds");
-        spawnRadius = getConfig().getInt("spawn-radius");
-        unvanishDelay = getConfig().getInt("unvanish-delay-ticks");
-        if (unvanishDelay == 0) {
-            unvanishDelay = 1;
-        }
-
         new ClearOldRequestsRunnable(this).runTaskTimer(this, 0, 20);
+
+    }
+
+    private int sanitizeConfigIntValue(String path) {
+
+        int value = getConfig().getInt(path);
+        if (value <= 0) {
+            value = (int) getConfig().getDefaults().get(path);
+            getConfig().set(path, value);
+            saveConfig();
+        }
+        return value;
 
     }
 
@@ -60,8 +63,6 @@ public final class SafeTP extends JavaPlugin {
         if (sender == null) {
             return false;
         }
-
-        // TODO: add timeout as command spam protection
 
         // 0 arg commands
 
@@ -108,7 +109,7 @@ public final class SafeTP extends JavaPlugin {
     }
 
     public void clearOldRequests() {
-        requestManager.clearOldRequests(timeoutValue);
+        requestManager.clearOldRequests(configRequestTimeoutSeconds);
     }
 
     public void unvanish(Player player) {
@@ -125,7 +126,7 @@ public final class SafeTP extends JavaPlugin {
             return;
         }
 
-        if (!tpaFromSpawn && isAtSpawn(tpRequester)) {
+        if (!configSpawnTpDeny && isAtSpawn(tpRequester)) {
             getLogger().info("Denying teleport request while in spawn area from " + tpRequester.getName() + " to " + tpTarget.getName());
             sendMessage(tpRequester, ChatColor.GOLD + "You are not allowed to teleport while in the spawn area!");
             return;
@@ -141,7 +142,7 @@ public final class SafeTP extends JavaPlugin {
             return;
         }
 
-        if (!multiRequest && requestManager.isRequestActiveByRequester(tpRequester)) {
+        if (!configMultiRequest && requestManager.isRequestActiveByRequester(tpRequester)) {
             sendMessage(tpRequester, ChatColor.GOLD + "Please wait for your existing request to be accepted or denied.");
             return;
         }
@@ -210,7 +211,6 @@ public final class SafeTP extends JavaPlugin {
 
         // execute teleport
         PaperLib.teleportAsync(tpRequester, tpTarget.getLocation()).thenAccept(result -> {
-
             if (result) {
                 sendMessage(tpTarget, tpRequester.getDisplayName() + ChatColor.GOLD + " teleported to you!");
                 sendMessage(tpRequester, ChatColor.GOLD + "Teleported to " + ChatColor.RESET + tpTarget.getDisplayName() + ChatColor.GOLD + "!");
@@ -218,11 +218,10 @@ public final class SafeTP extends JavaPlugin {
                 sendMessage(tpTarget, ChatColor.RED + "Teleport failed, you should harass your admin because of this!");
                 sendMessage(tpRequester, ChatColor.RED + "Teleport failed, you should harass your admin because of this!");
             }
-
         });
 
         // unvanish requester after n ticks
-        new UnvanishRunnable(this, tpRequester).runTaskLater(this, unvanishDelay);
+        new UnvanishRunnable(this, tpRequester).runTaskLater(this, configUnvanishDelay);
 
     }
 
@@ -233,7 +232,7 @@ public final class SafeTP extends JavaPlugin {
         }
 
         if (isToggled(toggleRequester)) {
-            getConfig().set(generateRequestBlockPath(toggleRequester), null); // if toggle is off, we delete instead of setting false
+            getConfig().set(generateRequestBlockPath(toggleRequester), null); // if toggle is getting turned off, we delete instead of setting false
             sendMessage(toggleRequester, ChatColor.GOLD + "Request are now " + ChatColor.GREEN + " enabled" + ChatColor.GOLD + "!");
         } else {
             getConfig().set(generateRequestBlockPath(toggleRequester), true);
@@ -283,7 +282,7 @@ public final class SafeTP extends JavaPlugin {
 
         boolean isNether = dim.equals(World.Environment.NETHER);
 
-        return Math.abs(loc.getX()) * (isNether ? 8 : 1) <= spawnRadius && Math.abs(loc.getZ()) * (isNether ? 8 : 1) <= spawnRadius;
+        return Math.abs(loc.getX()) * (isNether ? 8 : 1) <= configSpawnTpDenyRadius && Math.abs(loc.getZ()) * (isNether ? 8 : 1) <= configSpawnTpDenyRadius;
 
     }
 
@@ -308,13 +307,23 @@ public final class SafeTP extends JavaPlugin {
     }
 
     private void loadConfig() {
+
         getConfig().addDefault("allow-multi-target-request", true);
-        getConfig().addDefault("allow-tp-from-spawn", true);
         getConfig().addDefault("request-timeout-seconds", 60);
-        getConfig().addDefault("spawn-radius", 1500);
         getConfig().addDefault("unvanish-delay-ticks", 20);
+        getConfig().addDefault("spawn-tp-deny", true);
+        getConfig().addDefault("spawn-tp-deny-radius", 1500);
+
         getConfig().options().copyDefaults(true);
+
         saveConfig();
+
+        configMultiRequest = getConfig().getBoolean("allow-multi-target-request");
+        configRequestTimeoutSeconds = sanitizeConfigIntValue("request-timeout-seconds");
+        configUnvanishDelay = sanitizeConfigIntValue("unvanish-delay-ticks");
+        configSpawnTpDeny = getConfig().getBoolean("spawn-tp-deny");
+        configSpawnTpDenyRadius = sanitizeConfigIntValue("spawn-tp-deny-radius");
+
     }
 
 }
