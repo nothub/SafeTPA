fa326272c720570f22688d9d4630c055a405a4d7
Florian Hübner <florian.huebner@cloudogu.com>
2023-07-07 01:34:54 +0200

    accept and deny cmd

--- src/main/java/not/hub/safetpa/commands/AcceptCmd.java
+++ src/main/java/not/hub/safetpa/commands/AcceptCmd.java
@@ -1,17 +1,37 @@
 package not.hub.safetpa.commands;
 
 import not.hub.safetpa.Plugin;
+import not.hub.safetpa.util.Players;
+import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 
-public class AcceptCmd extends Command {
-    public AcceptCmd(PluginCommand pluginCommand) {
-        super(pluginCommand, 1, 1);
+// tpy (tpaccept)
+public class AcceptCmd extends TpCommand {
+    public AcceptCmd(Plugin plugin, PluginCommand pluginCommand) {
+        super(plugin, pluginCommand);
     }
 
     @Override
-    public boolean run(Plugin plugin, Player sender, String... args) {
-        // TODO
-        return false;
+    public boolean run(Player tpTarget, String requesterName) {
+        var tpRequester = Players.getOnlinePlayer(plugin.getServer(), requesterName);
+        if (tpRequester == null) {
+            tpTarget.sendMessage(ChatColor.GOLD + "Player " + ChatColor.RESET + requesterName + ChatColor.GOLD + " is not online.");
+            return false;
+        }
+
+        if (!plugin.requestManager().isRequestActive(tpTarget, tpRequester)) {
+            tpTarget.sendMessage(ChatColor.GOLD + "There is no request to accept from " + ChatColor.RESET + tpRequester.getName() + ChatColor.RESET + ChatColor.GOLD + "!");
+            return false;
+        }
+
+        tpTarget.sendMessage(ChatColor.GOLD + "Request from " + ChatColor.RESET + tpRequester.getName() + ChatColor.RESET + ChatColor.GREEN + " accepted" + ChatColor.GOLD + "!");
+        tpRequester.sendMessage(ChatColor.GOLD + "Your request was " + ChatColor.GREEN + "accepted" + ChatColor.GOLD + ", teleporting to: " + ChatColor.RESET + tpTarget.getName());
+
+        // TODO: combine these 2 methods to a single "accept" call
+        plugin.executeTP(tpTarget, tpRequester);
+        plugin.requestManager().removeRequests(tpTarget, tpRequester);
+
+        return true;
     }
 }
--- src/main/java/not/hub/safetpa/commands/DenyCmd.java
+++ src/main/java/not/hub/safetpa/commands/DenyCmd.java
@@ -1,17 +1,36 @@
 package not.hub.safetpa.commands;
 
 import not.hub.safetpa.Plugin;
+import not.hub.safetpa.util.Players;
+import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 
-public class DenyCmd extends Command {
-    public DenyCmd(PluginCommand pluginCommand) {
-        super(pluginCommand, 1, 1);
+// tpn (tpdeny)
+public class DenyCmd extends TpCommand {
+    public DenyCmd(Plugin plugin, PluginCommand pluginCommand) {
+        super(plugin, pluginCommand);
     }
 
     @Override
-    public boolean run(Plugin plugin, Player sender, String... args) {
-        // TODO
-        return false;
+    public boolean run(Player tpTarget, String requesterName) {
+        var tpRequester = Players.getOnlinePlayer(plugin.getServer(), requesterName);
+        if (tpRequester == null) {
+            tpTarget.sendMessage(ChatColor.GOLD + "Player " + ChatColor.RESET + requesterName + ChatColor.GOLD + " is not online.");
+            return false;
+        }
+
+        if (!plugin.requestManager().isRequestActive(tpTarget, tpRequester)) {
+            tpTarget.sendMessage(ChatColor.GOLD + "There is no request to deny from " + ChatColor.RESET + tpRequester.getName() + ChatColor.GOLD + "!");
+            return false;
+        }
+
+        tpTarget.sendMessage(ChatColor.GOLD + "Request from " + ChatColor.RESET + tpRequester.getName() + ChatColor.RESET + ChatColor.RED + " denied" + ChatColor.GOLD + "!");
+        tpRequester.sendMessage(ChatColor.GOLD + "Your request sent to " + ChatColor.RESET + tpTarget.getName() + ChatColor.RESET + ChatColor.GOLD + " was" + ChatColor.RED + " denied" + ChatColor.GOLD + "!");
+
+        // TODO: wrap this method in a "deny" call
+        plugin.requestManager().removeRequests(tpTarget, tpRequester);
+
+        return true;
     }
 }
